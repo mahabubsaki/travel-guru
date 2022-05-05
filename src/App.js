f73d66cd97866fd9b5df5b16a502dd4701f1f635
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-05-05 22:41:24 +0600

    placed background img and placed nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-     
+      <Home></Home>
     </div>
   );
 }
