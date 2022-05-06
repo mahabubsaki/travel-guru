4d66d0fdf23a5f09440a847fd554121923105f7a
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-05-06 20:14:52 +0600

    navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,25 +6,34 @@ const Navbar = () => {
         e.target.reset()
     }
     return (
-        <nav className="navbar navbar-expand-lg navbar-transparent bg-transparent">
+        <nav className="navbar navbar-expand-md navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <img src="https://i.ibb.co/ZxSdMdr/logo.png" alt="" width='100px' />
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
-                        </li>
-                    </ul>
-                    <form className="d-flex" onSubmit={handleSearch}>
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                    <form className="d-flex ms-0 mt-2 ms-md-4 mt-md-0" onSubmit={handleSearch}>
+                        <input className="form-control me-2" type="search" placeholder="Search your Destination" aria-label="Search" />
                         <button className="btn btn-outline-success" type="submit">Search</button>
                     </form>
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">News</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Destination</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Blogs</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Login</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
